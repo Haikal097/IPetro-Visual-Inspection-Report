836d58759cc5c1ab17f274a61df483d772ce0fde
Afiq Haikal <afiqh101@gmail.com>
2026-01-13 15:22:51 +0800

    Removed two factor auth

--- resources/js/layouts/settings/layout.tsx
+++ resources/js/layouts/settings/layout.tsx
@@ -25,11 +25,6 @@ const sidebarNavItems: NavItem[] = [
         icon: null,
     },
     {
-        title: 'Two-Factor Auth',
-        href: show(),
-        icon: null,
-    },
-    {
         title: 'Appearance',
         href: editAppearance(),
         icon: null,
